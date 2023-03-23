9313d717dc76ebf832842857d4b091986e0db4a0
joaqu1m <joaquim.pires@sptech.school>
2023-03-22 23:09:25 -0300

    estrutura remontada

---
+++ my-app-2/client/src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import React from 'react';
+//https://rapidapi.com/blog/create-react-app-express/
+function App() {
+  const [word, setWord] = React.useState('software');
+  const [associations, setAssociations] = React.useState(null);
+  const getAssociations = () => {
+    fetch('/api/associations/' + word)
+      .then(result => result.json())
+      .then(body => setAssociations(body));
+  };
+  return (
+    <div className="app">
+      <h1>Word Associations Map</h1>
+      <input value={word} onChange={e => setWord(e.target.value)} />
+      <button onClick={getAssociations}>Find Associations</button>
+      {associations && (
+        Object.keys(associations).length === 0
+          ? <p>No results</p>
+          : <div>
+            {Object.entries(associations).map(([association, score]) => (
+              <span style={{ fontSize: Math.pow(score, 2) / 200 }}>
+                {association}
+                {' '}
+              </span>
+            ))}
+          </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
