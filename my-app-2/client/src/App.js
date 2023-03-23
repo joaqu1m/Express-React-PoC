23ad3a21bfcc414ec8ce1841dc7d9a868d636fcc
joaqu1m <joaquim.pires@sptech.school>
2023-03-23 00:01:13 -0300

    react+express funfando

--- my-app-2/client/src/App.js
+++ my-app-2/client/src/App.js
@@ -1,33 +1,21 @@
-import './App.css';
-import React from 'react';
-//https://rapidapi.com/blog/create-react-app-express/
+import {React, useState} from 'react';
+import axios from 'axios'
+
 function App() {
-  const [word, setWord] = React.useState('software');
-  const [associations, setAssociations] = React.useState(null);
-  const getAssociations = () => {
-    fetch('/api/associations/' + word)
-      .then(result => result.json())
-      .then(body => setAssociations(body));
-  };
-  return (
-    <div className="app">
-      <h1>Word Associations Map</h1>
-      <input value={word} onChange={e => setWord(e.target.value)} />
-      <button onClick={getAssociations}>Find Associations</button>
-      {associations && (
-        Object.keys(associations).length === 0
-          ? <p>No results</p>
-          : <div>
-            {Object.entries(associations).map(([association, score]) => (
-              <span style={{ fontSize: Math.pow(score, 2) / 200 }}>
-                {association}
-                {' '}
-              </span>
-            ))}
-          </div>
-      )}
-    </div>
-  );
+    const [resposta, setResposta] = useState("")
+    axios.get("http://localhost:3001/")
+    .then(function (res) {
+        setResposta(res.data)
+    })
+    .catch(function (err) {
+        setResposta(err.data)
+    })
+    return (
+        <div className="app">
+            <h1>Word Associations Map</h1>
+            <h2>{resposta}</h2>
+        </div>
+    );
 }
 
 export default App;
